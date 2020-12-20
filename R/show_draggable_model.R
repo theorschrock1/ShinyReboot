75c61ad883ead8e6a4faff531122fcb7f53dea24
theordore schrock <theorschrock@gmail.com>
2020-12-20 14:51:29 -0700

    added show_draggable_model.R

---
+++ R/show_draggable_model.R
@@ -0,0 +1,20 @@
+#' show or hide a draggable model.
+
+#' @name show_draggable_model
+#' @param inputId  \code{[string]}  Defaults to \code{NULL}
+#' @param top  \code{[number]}  NULL is ok.  Defaults to \code{NULL}
+#' @param left  \code{[number]}  NULL is ok.  Defaults to \code{NULL}
+#' @param session  \code{[NULL]}  Defaults to \code{getDefaultReactiveDomain()}.
+#' @return \code{show_draggable_model}: invisible(NULL)
+#' @export
+show_draggable_model <- function(inputId = NULL, top = NULL, left = NULL, 
+    session = getDefaultReactiveDomain()) {
+    # show or hide a draggable model
+    assert_string(inputId)
+    assert_number(top, null.ok = TRUE)
+    assert_number(left, null.ok = TRUE)
+    session$sendInputMessage(inputId, value = drop_nulls(list(show = TRUE, 
+        top = top, left = left)))
+    invisible(NULL)
+    # Returns: invisible(NULL)
+}
