70e2494896e69f20a945aa66bf266478d106777f
theordore schrock <theorschrock@gmail.com>
2020-12-25 16:47:13 -0700

    added folder_collapse.R

---
+++ R/folder_collapse.R
@@ -0,0 +1,32 @@
+#' Create a collapsible folder.
+
+#' @name folder_collapse
+#' @param ...
+#' @param inputId  \code{[string]}
+#' @param label  \code{[string]}  NULL is ok.
+#' @param open  \code{[logical]}  Must have an exact length of \code{1}.  Defaults to \code{FALSE}
+#' @param open_icon  \code{[string]}  NULL is ok.  Defaults to \code{'folder-outline'}
+#' @param closed_icon  \code{[string]}  NULL is ok.  Defaults to \code{'folder-open-outline'}
+#' @return \code{folder_collapse}: \code{[html]}
+#' @examples
+
+#'  folder_collapse(inputId = 'myid', label = 'folder1', div('one'),
+#'  div('two'))
+#' @export
+folder_collapse <- function(..., inputId, label, open = FALSE, open_icon = "folder-outline", 
+    closed_icon = "folder-open-outline") {
+    # Create a collapsible folder
+    assert_string(inputId)
+    assert_string(label, null.ok = TRUE)
+    assert_logical(open, len = 1)
+    assert_string(open_icon, null.ok = TRUE)
+    assert_string(closed_icon, null.ok = TRUE)
+    collapse = "collapse"
+    content <- div(id = paste0(inputId, "collapse"), class = collapse, ...)
+    tagList(flexRow(class = "mdi-md align-items-center text-center", div(icon_mdi("chevron-right")), 
+        icon_toggler(inputId = inputId, value = open, icon_false = closed_icon, icon_true = open_icon, 
+            class = "icon", `data-toggle` = "collapse", `aria-expanded` = js_logical(open), 
+            `data-target` = paste0("#", inputId, "collapse"), span(class = "folder-label", 
+                label))), content)
+    # Returns: \code{[html]}
+}
